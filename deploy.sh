d474d392196ae5fe33556565e0cbf37f65b26b8a
Lart2P <1304992155@qq.com>
2019-03-15 09:26:07 +0800

    gengxin

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 if [ "$1" = "-i" ]
 then
-    mkdir output
-    cd output
-    git clone -b gh-pages git@github.com:Lart2P/wiki.git ./
-    cd ..
+    git clone https://github.com/LarT2P/wiki ./wiki
+    cd wiki/
     exit 0
 elif [ "$1" = "" ]
 then
     echo deploy [Option]
     echo "       -i 初始化"
     echo "       message  提交到github并发布，提交信息为mesage"
-    echo -e '*.pyc\noutput' > .gitignore
+    echo '*.pyc\noutput/' > .gitignore
     exit 0
 else
     simiki g
-    cp -r output/ docs/
+    cd output
+    cp -r . docs/
+    cd ..
     git add .
     git commit -m "$1"
     git pull origin master
